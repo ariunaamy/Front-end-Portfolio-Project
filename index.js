0d8ab57c71383e7fa6bdc1a76a84847bc948f652
ariunaamy <ariun.myagmar@gmail.com>
2023-01-14 00:38:46 -0500

    lists js structure added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const searchForm = document.querySelector(".searchForm");
 
+// setting up veriables for html elements 
+const searchForm = document.querySelector(".searchForm");
 const base_URL = "https://www.googleapis.com/books/v1/volumes?q=";
 const api_key = "&key=AIzaSyC-bwy6MNbTzusK3CjqU6Kk8dIpqVxG7Bk";
 
+// book card variables 
 const bookCard = document.querySelector(".bookCard");
 const description = document.querySelector(".description");
 const bookName = document.querySelector(".bookName");
@@ -14,84 +16,135 @@ const hidden = document.querySelector(".hidden")
 const addLists = document.getElementById("addLists")
 const addButton = document.querySelector("button")
 
+let listOfItems = [[], [], []];
+let firstList = listOfItems[0];
+let secondList = listOfItems[1];
+let thirdList = listOfItems[2];
 
 
 
+//Search Book event listner 
 searchForm.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    const book = document.querySelector('input').value;
-    getBookInfo(book); 
+  event.preventDefault();
+  const book = document.querySelector('input').value;
+  getBookInfo(book); 
   searchForm.reset();
-})
+ }
+)
 
-
+// function accessing Google Books API
 function getBookInfo(book) {
 fetch(base_URL+ book + api_key)
 .then(response => response.json())
 .then(result => {
-createCard(result);
-})
+  createCard(result);
+  }
+ )
 }
 
-
-
+//creating of book description card
 function createCard(result){
   hidden.style.visibility = "visible";
 
- bookName.innerHTML = result.items[0].volumeInfo.title
- author.innerHTML = "by " + result.items[0].volumeInfo.authors;
- 
+  bookName.innerHTML = result.items[0].volumeInfo.title
+  author.innerHTML = "by " + result.items[0].volumeInfo.authors;
 
- description.innerHTML = result.items[0].volumeInfo.description;
+  description.innerHTML = result.items[0].volumeInfo.description;
 
- thumbnail.setAttribute("src", result.items[0].volumeInfo.imageLinks.thumbnail);
- thumbnail.setAttribute("alt", "thumbnail of the book");
+  thumbnail.setAttribute("src", result.items[0].volumeInfo.imageLinks.thumbnail);
+  thumbnail.setAttribute("alt", "thumbnail of the book");
 
- 
-//  when a list is selected
- addButton.addEventListener("click", () =>{
   let title = result.items[0].volumeInfo.title;
- let id = result.items[0].id;
-  localStorage.setItem(id, title);
-  // let author = " by " + result.items[0].volumeInfo.authors;
+  let id = result.items[0].id;
 
+  let obj = {};
+  obj[id] = title;
+
+  //  when a list is selected
+  addButton.addEventListener("click", () => {
+  // options to add to lists 
   if (addLists.selectedIndex === 0){
-  let li = document.createElement("li");
-  let ul = document.querySelector(".firstList");
-  ul.append(li)
-  li.innerHTML = localStorage.getItem(id);
-   }
-  if (addLists.selectedIndex === 1){
-    
-  }
-  if (addLists.selectedIndex === 2){
-    
+    if (!firstList.some(obj => obj[id] === title) && !secondList.some(obj => obj[id] === title) && !thirdList.some(obj => obj[id] === title)) {
+      firstList.push(obj);
+      localStorage.setItem("added", JSON.stringify(listOfItems))
+      let storage = JSON.parse(localStorage.getItem('added'))
+      for (let i = 0; i < storage[0].length; i++) {
+        let li = document.createElement("li")
+        let ul = document.querySelector(".firstList")
+        ul.append(li);
+        li.innerHTML = Object.values(storage[0][i])[0]
+      }
+      //  console.log(obj)
     }
-  
-  })
+  }
 
+  else if (addLists.selectedIndex === 1) {
+    if (!firstList.some(obj => obj[id] === title) && !secondList.some(obj => obj[id] === title) && !thirdList.some(obj => obj[id] === title)) {
+      secondList.push(obj);
+      localStorage.setItem("added", JSON.stringify(listOfItems))
+      let storage = JSON.parse(localStorage.getItem('added'))
+      for (let i = 0; i < storage[1].length; i++) {
+        let li = document.createElement("li")
+        let ul = document.querySelector(".secondList")
+        ul.append(li);
+        li.innerHTML = Object.values(storage[1][i])[0]
+      }
+    }
+  }
+  else if (addLists.selectedIndex === 2) {
+    if (!firstList.some(obj => obj[id] === title) && !secondList.some(obj => obj[id] === title) && !thirdList.some(obj => obj[id] === title)) {
+      thirdList.push(obj);
+      localStorage.setItem("added", JSON.stringify(listOfItems))
+      let storage = JSON.parse(localStorage.getItem('added'))
+      for (let i = 0; i < storage[2].length; i++) {
+        let li = document.createElement("li")
+        let ul = document.querySelector(".thirdList")
+        ul.append(li);
+        li.innerHTML = Object.values(storage[2][i])[0]
+      } 
+      }
+    }
+  }
+  )
 }
 
-// let arrStorage = Object.values(localStorage);
-
-
+// adding books to lists 
 if (localStorage.length > 0){
-for (let i=0; i=localStorage.length; i++){
-let KeyName = window.localStorage.key(i);
-let li = document.createElement("li")
-document.querySelector(".firstList").append(li);
-li.innerHTML = localStorage.getItem(KeyName)
-}
+  document.querySelector(".one").remove()
+  document.querySelector(".two").remove()
+  document.querySelector(".three").remove()
+
+  let storage = JSON.parse(localStorage.getItem('added'));
+  for (let i = 0; i < storage[0].length; i++) {
+    let li = document.createElement("li")
+    let ul = document.querySelector(".firstList")
+    ul.append(li);
+    li.innerHTML = Object.values(storage[0][i])[0]
+  }
+  for (let i = 0; i < storage[1].length; i++) {
+    let li = document.createElement("li")
+    let ul = document.querySelector(".secondList")
+    ul.append(li);
+    li.innerHTML = Object.values(storage[1][i])[0]
+  }
+  for (let i = 0; i < storage[2].length; i++) {
+    let li = document.createElement("li")
+    let ul = document.querySelector(".thirdList")
+    ul.append(li);
+    li.innerHTML = Object.values(storage[2][i])[0]
+  }
 }
 
-//   document.querySelector(".one").innerHTML = "&#x2022 " + title;
-  //   document.querySelector(".one").style.color = "black";
-  //   let p =  document.createElement("p")
-  //   document.querySelector(".one").append(p)
-  //  p.innerHTML = author;
-  //  p.style.fontSize = "small";
-  //  p.style.fontStyle = "italic";
-  //  p.style.padding = "none";
+
+
+
+
+
+
+
+
+
+
 
 
 
